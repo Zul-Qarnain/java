ce5a42e110e129dcea306be7f294c2748a0284a1
Mohammd Shihab Hossain <shihab.hossain.2003@gmail.com>
2024-05-11 14:16:17 +0000

    Allhamduliha

---
+++ mycode.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Output{
+	public static void main(String[] args){
+	Scanner myobj = new Scanner(System.in);
+	System.out.println("Enter username:" );
+
+	String username= myobj.nextLine();
+	System.out.println("Usernmae: "+username);
+}
+}
